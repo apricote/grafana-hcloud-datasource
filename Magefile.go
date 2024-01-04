35b5f128a2aa5d0c467348aca2b4766f7684ac3e
Julian Tölle <julian.toelle97@gmail.com>
2024-01-04 15:43:39 +0100

    chore: dev setup in one command

--- Magefile.go
+++ Magefile.go
@@ -6,7 +6,21 @@ package main
 import (
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
 )
 
 // Default configures the default target.
 var Default = build.BuildAll
+
+func Dev() {
+	mg.Deps(build.Watch, DevFrontend, DevDockerCompose)
+}
+
+func DevFrontend() {
+	sh.RunV("npm", "run", "dev")
+}
+
+func DevDockerCompose() {
+	sh.RunV("docker", "compose", "up")
+}
